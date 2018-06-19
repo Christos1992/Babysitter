55e95b4f36eb27cbca9ccdde251ec61423ba7db4
christos angelopoulos <ch.angelopoulos@hotmail.com>
2018-06-19 15:15:04 +0200

    made a link from home page to all cards. made an index and a show View for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,18 @@ class PagesController < ApplicationController
   #skip_before_action :authenticate_user! , only: [:home]
   #skip_before_action :check_profile , only: [:home]
   def home
+
   end
+
+  def index
+    @city = params[:city]
+    @bbsitters = Bbsitter.where(city: params[:city])
+  end
+
+  def show
+    @bbsitters = Bbsitter.where(city: "Athens")
+  end
+
+
+
 end
