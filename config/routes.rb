84acc6daf005d5c0274ab705861f2a705f5d449e
Arnau Izard Torredemer <arnau.izardtorredemer@solvaypostgrad.net>
2018-06-21 11:57:09 +0200

    bbsitter showpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: "registrations" }
-  root to: 'pages#home'
+
   resources :parents, only:[:edit, :show, :update, :destroy]
   resources :bbsitters, only:[:edit, :show, :update, :destroy]
   resources :pages, only:[:show, :index]
+  authenticated :user do
+  root to: 'pages#home', as: :root #-> if user is logged in
+
+  end
+
+  unauthenticated :user do
+  root 'pages#home', as: :unauthenticated #-> if user is not logged in
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
